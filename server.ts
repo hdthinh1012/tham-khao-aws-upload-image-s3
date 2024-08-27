0663505ddc64156f5f7998621a010fe13b8e7a0a
hdthinh1012 <hdthinh01@gmail.com>
2024-08-28 06:49:59 +0700

    first commit

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { router as Router } from "./routes";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 80;
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://${app.head.name}:${port}`);
+});
